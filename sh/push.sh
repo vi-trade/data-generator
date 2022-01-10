092d8da44620a8c5d5895fb9d2f7266c44b6d7ee
vadim-ivlev <vadim.ivlev@gmail.com>
2022-01-10 06:12:47 +0300

    .

--- sh/push.sh
+++ sh/push.sh
@@ -9,11 +9,11 @@ echo
 git add -A .
 git commit -m "$msg."
 
-# git push gitlab --all #master
-# git push github --all #master
 git push origin --all #master
+# git push gitlab --all #master
+git push github --all #master
 
 
-# git push gitlab --tags 
-# git push github --tags 
 git push origin --tags 
+# git push gitlab --tags 
+git push github --tags 
